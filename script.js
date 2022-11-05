42b01e3933c882e87de802ae9889eacfb0690219
Maitlend <Maitlend.merrill@gmail.com>
2022-11-05 19:52:40 -0400

    Add functionality to digit and clear buttons

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+const oneButton = document.getElementById("one-btn");
+const twoButton = document.getElementById("two-btn");
+const threeButton = document.getElementById("three-btn");
+const fourButton = document.getElementById("four-btn");
+const fiveButton = document.getElementById("five-btn");
+const sixButton = document.getElementById("six-btn");
+const sevenButton = document.getElementById("seven-btn");
+const eightButton = document.getElementById("eight-btn");
+const nineButton = document.getElementById("nine-btn");
+
+const clearButton = document.getElementById("ac-btn");
+
+oneButton.addEventListener('click', digitHandler.bind(oneButton));
+twoButton.addEventListener('click', digitHandler.bind(twoButton));
+threeButton.addEventListener('click', digitHandler.bind(threeButton));
+fourButton.addEventListener('click', digitHandler.bind(fourButton));
+fiveButton.addEventListener('click', digitHandler.bind(fiveButton));
+sixButton.addEventListener('click', digitHandler.bind(sixButton));
+sevenButton.addEventListener('click', digitHandler.bind(sevenButton));
+eightButton.addEventListener('click', digitHandler.bind(eightButton));
+nineButton.addEventListener('click', digitHandler.bind(nineButton));
+
+clearButton.addEventListener('click', clearButtonHandler);
+
+const display = document.getElementById("display");
+// let displayContent = 0;
+let displayConentHolder = 0;
+
+function digitHandler(){
+  console.log(parseInt(this.textContent));
+  updateDisplay(parseInt(this.textContent));
+}
+
+function clearButtonHandler(){
+  updateDisplay(0);
+}
+
+function updateDisplay(content){
+  const displayContent = document.createElement('p');
+  displayContent.setAttribute("style", "font-size: 10vw margin: 0;");
+  displayContent.textContent = content;
+  display.replaceChild(displayContent,display.firstElementChild);
+}
+
 function add(a,b){
   return a+b;
 }
